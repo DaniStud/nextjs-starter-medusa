61cd69acd90d423f9cdd89e059d05b6bf9ac6449
Daniel Wirenfeldt <wirenfeldtd@gmail.com>
2025-12-20 20:38:29 +0100

    add-hero-image

--- src/modules/home/components/hero/index.tsx
+++ src/modules/home/components/hero/index.tsx
@@ -3,29 +3,29 @@ import { Button, Heading } from "@medusajs/ui"
 
 const Hero = () => {
   return (
-    <div className="h-[75vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle">
+    <div className="h-[75vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle bg-[url(/images/laneige-01.jpg)] bg-cover">
       <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6">
         <span>
           <Heading
             level="h1"
             className="text-3xl leading-10 text-ui-fg-base font-normal"
           >
-            Ecommerce Starter Template
+            Take care of your skin
           </Heading>
           <Heading
             level="h2"
             className="text-3xl leading-10 text-ui-fg-subtle font-normal"
           >
-            Powered by Medusa and Next.js
+            We will keep you protected all year round.
           </Heading>
         </span>
+
         <a
-          href="https://github.com/medusajs/nextjs-starter-medusa"
+          href="http://localhost:8000/gb/store"
           target="_blank"
         >
-          <Button variant="secondary">
-            View on GitHub
-            <Github />
+          <Button className="w-fit mt-16" variant="primary">
+            Shop Now
           </Button>
         </a>
       </div>
@@ -34,3 +34,4 @@ const Hero = () => {
 }
 
 export default Hero
+
